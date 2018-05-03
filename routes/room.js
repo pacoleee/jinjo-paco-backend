8ed953185258db2644e6c4bf1519a6588190ffd3
Paco Lee <paco.lee@twotalltotems.com>
2018-05-03 15:39:36 -0700

    add room

--- routes/room.js
+++ routes/room.js
@@ -10,7 +10,23 @@ var connection = require('../server').connection;
 
 // API functions
 function addRoom(request, response) {
+	var instructorID = request.body.instructorID;
+	var roomName = request.body.roomName;
 
+	var query = 'INSERT INTO Room (instructorID, roomName) \
+    Values ("' +instructorID +'", "' +roomName +'")';
+
+    connection.query(query, function(error, rows, fields){
+    	if(!!error) {
+    		console.log('Error in the query\n');
+                response.status(422);
+                response.send('422 Unprocessable Entity');
+                return;
+            }
+     	else {
+      		response.send(rows);
+     	}
+  	});
 }
 
 function deleteRoom(request, response) {
