76c7abadfac58d9b994e9b9c9d8d2450aee26acc
Paco Lee <paco.lee@twotalltotems.com>
2018-05-04 16:14:46 -0700

    Re indented and fixed bugs

--- routes/instructor.js
+++ routes/instructor.js
@@ -1,9 +1,9 @@
 //routes
 module.exports = function(app) {
-    app.route('/instructor')
-        .post(addInstructor)
-        .put(updatePassword)
-        .get(loginInstructor);
+  app.route('/instructor')
+  .post(addInstructor)
+  .put(updatePassword)
+  .get(loginInstructor);
 }
 
 var connection = require('../server').connection;
@@ -14,74 +14,72 @@ function addInstructor(request, response) {
 	var password = request.body.password;
 
 	var query = 'INSERT INTO Instructor (email, password) \
-    Values ("' +email +'", "' +password +'")';
+  Values ("' +email +'", "' +password +'")';
 
-    console.log(query);
+  console.log(query);
 
-    connection.query(query, function(error, rows, fields){
-    	if(!!error) {
-    		console.log('Error in the query\n');
-                response.status(422);
-                response.send('422 Unprocessable Entity');
-                return;
-            }
-     	else {
-      		response.send(rows);
-     	}
-  	});
+  connection.query(query, function(error, rows, fields){
+   if(!!error) {
+    console.log('Error in the query\n');
+    response.status(422);
+    response.send('422 Unprocessable Entity');
+    return;
+    } else {
+      response.send(rows);
+    }
+  });
 }
 
 function updatePassword(request, response) {
 	var email = request.body.email;
-	var oldPassword = request.body.oldPassword;
-    var newPassword = request.body.newPassword;
+  var oldPassword = request.body.oldPassword;
+  var newPassword = request.body.newPassword;
 
 
-    var selectQuery = 'SELECT * FROM Instructor \
-    WHERE email ="' +email +'" AND password = "' +oldPassword +'"';
+  var selectQuery = 'SELECT * FROM Instructor \
+  WHERE email ="' +email +'" AND password = "' +oldPassword +'"';
 
-	var updateQuery = 'UPDATE Instructor \
-    Set password="' +newPassword +'" \
-    WHERE email = "' +email +'"';
+  var updateQuery = 'UPDATE Instructor \
+  Set password="' +newPassword +'" \
+  WHERE email = "' +email +'"';
 
-    connection.query(selectQuery, function(error, rows, fields){
-    	if(!!error) {
-    		console.log('Error in the query\n');
-                response.status(422);
-                response.send('422 Unprocessable Entity');
-                return;
-            }
-     	else {
-      		connection.query(updateQuery, function(error, rows, fields){
-    			if(!!error) {
-    				console.log('Error in the query\n');
-             	   response.status(422);
-             	   response.send('422 Unprocessable Entity');
-             	   return;
-           		} else {
-     				response.send(rows);
-     			}
-  			});
-     	}
-  	});
+  connection.query(selectQuery, function(error, rows, fields){
+    if(!!error) {
+      console.log('Error in the query\n');
+      response.status(422);
+      response.send('422 Unprocessable Entity');
+      return;
+    } else {
+      connection.query(updateQuery, function(error, rows, fields){
+       if(!!error) {
+        console.log('Error in the query\n');
+        response.status(422);
+        response.send('422 Unprocessable Entity');
+        return;
+        } else {
+         response.send(rows);
+        }
+      });
+    }
+  });
 }
 
 function loginInstructor(request, response) {
-    var email = request.body.email;
-    var oldPassword = request.body.password;
+  var email = request.body.email;
+  var password = request.body.password;
 
-    var selectQuery = 'SELECT * FROM Instructor \
-    WHERE email ="' +email +'" AND password = "' +password +'"';
+  var selectQuery = 'SELECT * FROM Instructor \
+  WHERE email ="' +email +'" AND password = "' +password +'"';
 
-    connection.query(selectQuery, function(error, rows, fields){
-        if(!!error) {
-            console.log('Error in the query\n');
-                response.status(422);
-                response.send('422 Unprocessable Entity');
-                return;
-            }
-        else {
-            response.send(rows);
-        }
-    });
+  connection.query(selectQuery, function(error, rows, fields){
+    if(!!error) {
+      console.log('Error in the query\n');
+      response.status(422);
+      response.send('422 Unprocessable Entity');
+      return;
+    }
+    else {
+      response.send(rows);
+    }
+  });
 }
--- routes/question.js
+++ routes/question.js
@@ -1,26 +1,92 @@
 //routes
 module.exports = function(app) {
-    app.route('/question')
-        .post(addQuestion);
+	app.route('/question')
+	.post(addQuestion);
+	.get(getQuestions);
 
-    app.route('/question/like')
-    	.post(likeQuestion);
+	app.route('/question/like/:id')
+	.put(likeQuestion);
 
-    app.route('/question/answer')
-    	.post(answerQuestion);
+	app.route('/question/answer/:id')
+	.put(answerQuestion);
 }
 
 var connection = require('../server').connection;
 
 // API functions
-function addQuestion(request, response) {
+function getQuestions(request, response) {
+	var roomID = request.body.roomID;
 
+	var query = 'SELECT * FROM Question \
+	WHERE roomID = ' +roomID;
+
+	connection.query(query, function(error, rows, fields){
+		if(!!error) {
+			console.log('Error in the query\n');
+			response.status(422);
+			response.send('422 Unprocessable Entity');
+			return;
+		} else {
+			response.send(rows);
+		}
+	});
+}
+
+function addQuestion(request, response) {
+	var question = request.body.question;
+	var roomID = request.body.roomID;
+
+	var query = 'INSERT INTO Question (question, upvotes, roomID) \
+	Values ("' +question +'", "' +roomID +'")';
+
+	connection.query(query, function(error, rows, fields){
+		if(!!error) {
+			console.log('Error in the query\n');
+			response.status(422);
+			response.send('422 Unprocessable Entity');
+			return;
+		} else {
+			response.send(rows);
+		}
+	});
 }
 
 function likeQuestion(request, response) {
+	var questionID = request.params.questionID;
 
+	var query = 'UPDATE Question \
+	SET upvotes = upvotes + 1 \
+	WHERE questionID = ' +questionID;
+
+
+	connection.query(query, function(error, rows, fields){
+		if(!!error) {
+			console.log('Error in the query\n');
+			response.status(422);
+			response.send('422 Unprocessable Entity');
+			return;
+		} else {
+			response.send(rows);
+		}
+	});
 }
 
 function answerQuestion(request, response) {
-	
+	var questionID = request.params.questionID;
+
+	var query = 'UPDATE Question \
+	SET isAnswered = 1 \
+	WHERE questionID = ' +questionID;
+
+
+	connection.query(query, function(error, rows, fields){
+		if(!!error) {
+			console.log('Error in the query\n');
+			response.status(422);
+			response.send('422 Unprocessable Entity');
+			return;
+		} else {
+			response.send(rows);
+		}
+	});
 }
